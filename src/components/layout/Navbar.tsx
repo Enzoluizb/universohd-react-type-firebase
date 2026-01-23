68fcc658029ee9727e4dc0a5ebd3430f45bcc7e7
Enzoluizb <enzoluiz.brt@gmail.com>
2026-01-22 23:15:46 -0300

    Header "feito"

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,9 +2,27 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="p-4 flex gap-4 border-b">
-      <Link to="/">Home</Link>
-      <Link to="/marketplace">Marketplace</Link>
+    <nav className="w-full bg-gray-100 border-b">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 flex items-center justify-between h-24">
+        <Link to="/" className="flex items-center">
+          <img src="/images/logo.png" alt="Logo" className="h-14 w-auto" />
+        </Link>
+
+        <Link
+          to="/marketplace"
+          className="
+            bg-gradient-to-r from-yellow-400 to-yellow-500
+            hover:from-yellow-500 hover:to-yellow-600
+            text-white text-sm font-semibold
+            px-4 py-2 rounded-md
+            leading-tight text-center
+            shadow-md transition-all
+          "
+        >
+          Acesso Especialistas, Master Minds e<br />
+          Embaixadoras HD
+        </Link>
+      </div>
     </nav>
   );
 }

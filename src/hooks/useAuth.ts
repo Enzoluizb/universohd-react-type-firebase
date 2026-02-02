3ddf3710db7e61a0565f0e63c6d427d996a36164
Enzoluizb <enzoluiz.brt@gmail.com>
2026-02-02 19:39:24 -0300

    Análise de código

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,3 +4,20 @@ import { AuthContext } from "../contexts/AuthContext";
 export function useAuth() {
     return useContext(AuthContext);
 }
+
+/* 
+
+SEM useauth
+
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
+const { user, logout } = useContext(AuthContext);
+
+COM useauth
+
+import { useAuth } from "../hooks/useAuth";
+
+const { user, logout } = useAuth();
+
+*/

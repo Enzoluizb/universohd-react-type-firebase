a62319b5593421312078bc11d1b3614dd2908b26
Enzoluizb <enzoluiz.brt@gmail.com>
2026-02-03 21:06:18 -0300

    Explicação e análise do conteúdo protected route

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -2,6 +2,8 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import React from "react";
 
+// Define o tipo das propriedades do componente
+// children representa qualquer componente JSX passado dentro do ProtectedRoute
 type Props = {
   children: React.ReactNode;
 };
@@ -10,7 +12,12 @@ export default function ProtectedRoute({ children }: Props) {
   const { user, loading } = useAuth();
 
   if (loading) return null;
+
+  // Se não existir um usuário logado,
+  // redireciona automaticamente para a página de login
   if (!user) return <Navigate to="/login" />;
 
+  // Se o usuário estiver autenticado,
+  // renderiza normalmente o conteúdo protegido
   return <>{children}</>;
 }

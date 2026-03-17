e8dbfcc2cabf58aa8129c2a5f1152df8eb0b2c50
Enzoluizb <enzoluiz.brt@gmail.com>
2026-03-16 23:13:26 -0300

    Ajuste Navbar (nomenclatura)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -106,7 +106,7 @@ export default function Navbar() {
               onClick={handleMarketplaceAccess}
               className="bg-gradient-to-r from-yellow-400 to-yellow-500 hover:from-yellow-500 hover:to-yellow-600 text-white text-sm font-semibold px-4 py-2 rounded-md"
             >
-              Acesso Especialistas
+              Acesso Especialistas, Master MindHD e Embaixadoras HD
             </button>
           )}
         </div>
@@ -168,7 +168,7 @@ export default function Navbar() {
               onClick={handleMarketplaceAccess}
               className="bg-yellow-500 text-white py-2 rounded-md"
             >
-              Acesso Especialistas
+              Acesso Especialistas, Master MindHD e Embaixadoras HD
             </button>
           )}
         </div>
